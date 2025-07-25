3fb1b0df1672390fa7593257ad8c77cbca1b9304
Nova-dotcom-code! <mustophaajibola70@gmail.com>
2025-07-25 08:44:54 +0100

    modified script.js added setInterval to automatically slide to the next image within the set interval

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
-const slider = document.querySelector('.slider');
-const images = document.querySelectorAll('.slider img');
-const prevBtn = document.querySelector('.prev');
-const nextBtn = document.querySelector('.next');
+const slider = document.querySelector(".slider");
+const images = document.querySelectorAll(".slider img");
+const prevBtn = document.querySelector(".prev");
+const nextBtn = document.querySelector(".next");
 let counter = 0;
-imageWidth = images[0].clientWidth
+imageWidth = images[0].clientWidth;
 
-function slideNext(){
-    counter++
-    if(counter>=images.length){
-        counter = 0;
-    }
-    slider.style.transform = `translate(${-imageWidth * counter}px)`;
+function slideNext() {
+  counter++;
+  if (counter >= images.length) {
+    counter = 0;
+  }
+  slider.style.transform = `translate(${-imageWidth * counter}px)`;
 }
-function slidePrev(){
-    counter--;
-    if(counter < 0){
-        counter = images.length - 1;
-    }
-    slider.style.transform = `translate(${-imageWidth * counter}px)`;
-};
-prevBtn.addEventListener('click', slidePrev);
-nextBtn.addEventListener('click', slideNext);
+function slidePrev() {
+  counter--;
+  if (counter < 0) {
+    counter = images.length - 1;
+  }
+  slider.style.transform = `translate(${-imageWidth * counter}px)`;
+}
+prevBtn.addEventListener("click", slidePrev);
+nextBtn.addEventListener("click", slideNext);
+setInterval(slideNext, 3000);
