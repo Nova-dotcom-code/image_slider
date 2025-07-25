51d32c42a4c636ea0cc59768ccd9af5cb00aa892
Nova-dotcom-code! <mustophaajibola70@gmail.com>
2025-07-25 08:17:29 +0100

    Add script.js with initial DOM element selection

---
+++ script.js
@@ -0,0 +1,4 @@
+const slider = document.querySelector('.slider');
+const images = document.querySelectorAll('.slider img');
+const prevBtn = document.querySelector('.prev');
+const nextBtn = document.querySelector('.next');
