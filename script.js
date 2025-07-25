b56d4fc0846c4f659c8d06c1fdab29ee45db51ca
Nova-dotcom-code! <mustophaajibola70@gmail.com>
2025-07-25 08:24:27 +0100

    Update script.js to improve functionality added a functionality for image to slide next

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ const slider = document.querySelector('.slider');
 const images = document.querySelectorAll('.slider img');
 const prevBtn = document.querySelector('.prev');
 const nextBtn = document.querySelector('.next');
+let counter = 0;
+imageWidth = images[0].clientWidth
+
+function slideNext(){
+    counter++
+    if(counter>=images.length){
+        counter = 0;
+    }
+    slider.style.transform = `translate(${-imageWidth * counter}px)`;
+}
